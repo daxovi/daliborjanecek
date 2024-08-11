e647a97287cfb866f4b6bf725debfa4f8d5ac986
Dalibor Janeček <dalibor.janecek@gmail.com>
2024-08-11 22:15:39 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import './App.css';
 import video_riva from './video/riva_tango_web.mp4'
+import img1 from "./img/img.jpg"
 
 function App() {
   return (
     <div className="container">
       <header>
-        <h1>Dalibor Janček: Visual experience</h1>
-      </header>
-      <div></div>
-      <div className='display'>
-          <video autoPlay muted loop>
-            <source src={video_riva} type="video/mp4" />
-          </video>
+      <div className="text">
+        <h1>
+          <span>Dalibor Janeček </span><br /> Visual experience
+        </h1>
+        <nav>
+          <a href="">video</a><a href="">motion</a><a href="">3d</a><a href="">app</a><a href="">design</a>
+        </nav>
+
       </div>
+      </header>
+      <div className='display'>
+        <video autoPlay muted loop>
+          <source src={video_riva} type="video/mp4" />
+        </video>
+      </div>
+      
+      <div className='display'>
+        <video autoPlay muted loop>
+          <source src={video_riva} type="video/mp4" />
+        </video>
+      </div>
+      <div className='display'>
+        <img src={img1} alt="" />
+      </div>
+      <div className='display'>
+        <video autoPlay muted loop>
+          <source src={video_riva} type="video/mp4" />
+        </video>
+      </div>
+      <div className='display'>
+        <video autoPlay muted loop>
+          <source src={video_riva} type="video/mp4" />
+        </video>
+      </div>
+      <div className='display'>
+        <video autoPlay muted loop>
+          <source src={video_riva} type="video/mp4" />
+        </video>
+      </div>
+
       <div className='display'></div>
       <div className='display'></div>
       <footer>
